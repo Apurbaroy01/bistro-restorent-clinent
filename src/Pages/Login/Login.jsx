8de9e5ad71e35e694f6acc29cb2f998160aaed59
Apurbaroy01 <apurbaroy00011@gmail.com>
2025-09-04 13:34:13 +0600

    11th

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,9 +1,24 @@
 import { Mail, Lock } from "lucide-react"; // lucide-react install করলে কাজ করবে
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa6";
+import { LoadCanvasTemplate, loadCaptchaEnginge } from "react-simple-captcha";
 
 const Login = () => {
-    const [eye, setEye] = useState()
+    const [eye, setEye] = useState();
+
+    useEffect(() => {
+        loadCaptchaEnginge(6);
+    }, [])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        console.log(email, password)
+
+    }
     return (
         <div className="min-h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 flex items-center justify-center p-4">
             <div className="card w-full max-w-md bg-white/90 backdrop-blur-lg shadow-2xl rounded-2xl">
@@ -12,36 +27,47 @@ const Login = () => {
                     <p className="text-center text-gray-600 mb-6">
                         Please login to your account to continue
                     </p>
+                    <form onSubmit={handleSubmit}>
+                        <fieldset className="space-y-4">
 
-                    <fieldset className="space-y-4">
-                        {/* Email Input */}
-                        <label className="input input-bordered flex items-center gap-2 w-full">
-                            <Mail className="w-5 h-5 text-gray-400" />
-                            <input type="email" className="grow" placeholder="Email" />
-                        </label>
+                            {/* Email Input */}
+                            <label className="input input-bordered flex items-center gap-2 w-full">
+                                <Mail className="w-5 h-5 text-gray-400" />
+                                <input type="email" className="grow" placeholder="Email" name="email" />
+                            </label>
 
-                        {/* Password Input */}
-                        <label className="input input-bordered flex items-center gap-2 w-full">
-                            <Lock className="w-5 h-5 text-gray-400" />
-                            <input type={eye ? "password": "text"} className="grow" placeholder="Password" />
-                            <p className="text-xl" onClick={() => setEye(!eye)}>
-                                {
-                                    eye ? <FaRegEyeSlash /> : <FaRegEye />
-                                }
+                            {/* Password Input */}
+                            <label className="input input-bordered flex items-center gap-2 w-full">
+                                <Lock className="w-5 h-5 text-gray-400" />
+                                <input type={eye ? "password" : "text"} className="grow" placeholder="Password" name="password" />
+                                <p className="text-xl" onClick={() => setEye(!eye)}>
+                                    {
+                                        eye ? <FaRegEyeSlash /> : <FaRegEye />
+                                    }
 
-                            </p>
-                        </label>
+                                </p>
+                            </label>
+                            <label className=" flex flex-col  gap-2 w-full">
+                                <LoadCanvasTemplate />
 
-                        <div className="flex justify-between items-center text-sm">
-                            <a className="link link-hover text-indigo-600">Forgot password?</a>
-                            <a className="link link-hover text-indigo-600">Create account</a>
-                        </div>
+                                <div className="input input-bordered flex items-center gap-2 w-full">
+                                    <input type="text" className="grow" placeholder="Type captcha"  />
+                                </div>
 
-                        {/* Login Button */}
-                        <button className="btn bg-indigo-600 hover:bg-indigo-700 text-white w-full mt-2">
-                            Login
-                        </button>
-                    </fieldset>
+                            </label>
+
+                            <div className="flex justify-between items-center text-sm">
+                                <a className="link link-hover text-indigo-600">Forgot password?</a>
+                                <a className="link link-hover text-indigo-600">Create account</a>
+                            </div>
+
+                            {/* Login Button */}
+                            <button className="btn bg-indigo-600 hover:bg-indigo-700 text-white w-full mt-2">
+                                Login
+                            </button>
+
+                        </fieldset>
+                    </form>
                 </div>
             </div>
         </div>
