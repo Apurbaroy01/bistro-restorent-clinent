df8ac922d2bfa8cf0c607ea11934edbb9d9dfa64
Apurbaroy01 <apurbaroy00011@gmail.com>
2025-09-07 15:19:10 +0600

    18th

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import useAuth from '../../Hookes/useAuth';
+import Swal from 'sweetalert2';
+import { useNavigate } from 'react-router-dom';
 
 const FoodCard = ({ item }) => {
     const { name, image, price, recipe } = item;
+    const { user } = useAuth();
+    const navigate = useNavigate();
+
+    const handleAddtoCart = (food) => {
+        if (user && user.email) {
+            console.log(food, user.email)
+        }
+        else {
+            Swal.fire({
+                title: "You are not Logged In",
+                text: "Please Login!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, Login!"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    navigate("/login")
+                }
+            });
+        }
+    }
 
 
     return (
@@ -18,7 +44,7 @@ const FoodCard = ({ item }) => {
                 <h2 className="card-title">{name}</h2>
                 <p>{recipe}</p>
                 <div className="card-actions justify-center">
-                    <button className="btn btn-primary">Add to card</button>
+                    <button onClick={() => handleAddtoCart(item)} className="btn btn-primary">Add to card</button>
                 </div>
             </div>
         </div>
