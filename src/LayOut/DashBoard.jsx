52995d57ba68411df80bf5c1d2810faa92e1d8b7
Apurbaroy01 <apurbaroy00011@gmail.com>
2025-09-08 14:59:21 +0600

    24th

--- src/LayOut/DashBoard.jsx
+++ src/LayOut/DashBoard.jsx
@@ -7,18 +7,22 @@ import { NavLink, Outlet } from "react-router-dom";
 const DashBoard = () => {
     return (
         <div className="flex">
-            <div className="w-64 min-h-screen bg-orange-400">
-                <ul className="menu">
+            <div className="w-64 min-h-screen bg-orange-400 ">
+                <ul className="menu space-y-2 w-full mt-5">
                     <li><NavLink to="/dashboard/userHome"><FaHome /> User Home</NavLink></li>
-                    
                     <li><NavLink to="/dashboard/reservation"><FaCalendar /> Reservation</NavLink></li>
-                    <li><NavLink to="/dashboard/userHome"><TiShoppingCart /> My cart</NavLink></li>   
+                    <li><NavLink to="/dashboard/cart"><TiShoppingCart /> My cart</NavLink></li>   
                     <li><NavLink to="/dashboard/reservation"><FaAd/> Add a review</NavLink></li>
                     <li><NavLink to="/dashboard/reservation"><FaList /> My Bookings</NavLink></li>
 
+                    <div className="divider"></div>
+
+                    <li><NavLink to="/"><FaHome /> Home </NavLink></li>
+
+
                 </ul>
             </div>
-            <div>
+            <div className="p-5">
                 <Outlet></Outlet>
             </div>
         </div>
