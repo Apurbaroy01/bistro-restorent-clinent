460b353b3a88b7e27d526c753ef4d75cd091d8db
Apurbaroy01 <apurbaroy00011@gmail.com>
2025-09-10 00:57:45 +0600

    32

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,12 +1,14 @@
 import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { createContext, useEffect, useState } from "react";
 import auth from "../firebase/firebase";
+import useAxiosPurblic from "../Hookes/useAxiosPurblic";
 
 export const AuthContext = createContext(null)
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null)
     const [loading, setLoading] = useState(true)
+    const axoiosPublic = useAxiosPurblic();
 
     const googleProvider = new GoogleAuthProvider();
 
@@ -43,10 +45,24 @@ const AuthProvider = ({ children }) => {
             console.log("Current email:", currentUser)
             setUser(currentUser);
             setLoading(false)
+            if (currentUser) {
+                const info = {
+                    email: currentUser.email
+                };
+                axoiosPublic.post('/jwt', info)
+                    .then(data => {
+                        if(data.data.token){
+                            localStorage.setItem('access-token', data.data.token);
+                        }
+                    })
+            }
+            else{
+                localStorage.removeItem('access-token');
+            }
 
         })
         return unsubscribe;
-    }, [])
+    }, [axoiosPublic])
 
     const AuthInfo = {
         googleSignIn,
